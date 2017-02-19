83f130bd4d64ccb6d650cdf61e7991dfa44eb793
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2017-02-19 17:35:15 -0500

    Add pipes and nested components

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { ProductListComponent } from './products/product-list.component';
+
 @Component({
     selector: 'pm-app',
     template: `
         <div class="container">
             <h1>{{pageTitle}}</h1>
-            <div class="well">My First Componeent</div>
+            <pm-products></pm-products>
         </div>
     `
 })
